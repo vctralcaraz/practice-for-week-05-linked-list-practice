c26966e116c4c87740207a8917fc44231fe40bd4
Victor Alcaraz <vctralcaraz@gmail.com>
2023-09-27 20:59:28 -0700

    completed phase 1

--- linked-list.js
+++ linked-list.js
@@ -8,14 +8,40 @@ class LinkedListNode {
 class LinkedList {
   constructor() {
     // Your code here
+    this.head = null;
+    this.length = 0;
   }
 
   addToHead(val) {
     // Your code here
+    const newNode = new LinkedListNode(val);  // create new node with given val
+
+    newNode.next = this.head;  // set new nodes 'next' pointer to the list's current head pointer
+
+    this.head = newNode;  //  set the list head to point to the new node
+    this.length += 1;  // increase length by 1
   }
 
   addToTail(val) {
     // Your code here
+    if(!this.head) {  // if head is NULL
+      this.addToHead(val);  // invoke addToHead function with val
+      return;
+    }
+
+    let current = this.head;  // set head to current
+    const newNode = new LinkedListNode(val);  // create a new node
+
+    while(current) {  // iterate through all nodes  // time complexity O(n)
+      if(current.next === null) {  // if the current node's next pointer is null, we have reached the end of the list
+        current.next = newNode;  // set the next pointer to the new node
+        this.length += 1;  // increase length by 1
+
+        break;  // break out of the loop
+      }
+
+      current = current.next;  // iterate to the next node
+    }
   }
 
   // You can use this function to help debug
