abb9a4b7e9a9709b621db7d9d25a710fdcfbdf95
Victor Alcaraz <vctralcaraz@gmail.com>
2023-09-27 21:43:19 -0700

    completed phase 3

--- linked-list-timing.js
+++ linked-list-timing.js
@@ -7,3 +7,42 @@ Construct a timing test to verify the time complexities of `addToHead` and
 */
 
 // Your code here
+const iterations = 10000;  // ten thousand
+let start, end;
+
+const ll = new LinkedList();
+const dll = new DoublyLinkedList();
+
+// Linked List addToHead x iterations
+start = new Date();
+for(let i = 0; i < iterations; i++) {
+  ll.addToHead(i);
+}
+end = new Date();
+console.log(`Linked List - addToHead x ${iterations}: ${end - start}ms`);
+
+// Linked List addToTails x iterations
+start = new Date();
+for(let i = 0; i < iterations; i++) {
+  ll.addToTail(i);
+}
+end = new Date();
+console.log(`Linked List - addToTail x ${iterations}: ${end - start}ms`);
+
+
+
+// Doubly Linked List addToHead x iterations
+start = new Date();
+for(let i = 0; i < iterations; i++) {
+  dll.addToHead(i);
+}
+end = new Date();
+console.log(`Doubly Linked List - addToHead x ${iterations}: ${end - start}ms`);
+
+// Doubly Linked List addToTail x iterations
+start = new Date();
+for(let i = 0; i < iterations; i++) {
+  dll.addToTail(i);
+}
+end = new Date();
+console.log(`Doubly Linked List - addToTail x ${iterations}: ${end - start}ms`);
